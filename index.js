e0d5c2894ffa8d2152b88348961b1690edd239cb
Tushar-ba <tusharba321@gmail.com>
2024-07-08 11:02:48 +0530

    created deposit method

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ class BankAccount{
         this.balance = balance;
         this.transactionHistory=transactionHistory;
     }
+
+    deposit(amount){
+        this.balance += amount;
+    }
+
 }
 
 let b1 = new BankAccount(123,"tus",100,[0,1,3]);
 
+b1.deposit(500);
 
 console.log(b1);
