736275c95d4c20312ff33cd274f0fb4f9699ca6c
Tushar-ba <tusharba321@gmail.com>
2024-07-08 11:12:59 +0530

    added get Account details

--- index.js
+++ index.js
@@ -18,11 +18,21 @@ class BankAccount{
         }
     }
 
+    getAccountDetails(){
+        return{
+            accountNumber:this.accountNumber,
+            owner:this.owner,
+            balance: this.balance,
+            transactionHistory:this.transactionHistory
+        }
+    }
+
 }
 
 let b1 = new BankAccount(123,"tus",100,[0,1,3]);
 
 b1.deposit(500);
-b1.withdraw(700)
+b1.withdraw(100);
+b1.getAccountDetails()
 
-console.log(b1);
+console.log(b1.getAccountDetails());
