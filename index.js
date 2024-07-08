68bb1ef4346ac9d9e9d8b63a0c710525f498265b
Tushar-ba <tusharba321@gmail.com>
2024-07-08 11:09:13 +0530

    created withdraw method

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ class BankAccount{
     deposit(amount){
         this.balance += amount;
     }
+    withdraw(amount){
+        if(amount<=this.balance){
+            this.balance -= amount;
+        }
+        else{
+            console.log("insufficient balance")
+        }
+    }
 
 }
 
 let b1 = new BankAccount(123,"tus",100,[0,1,3]);
 
 b1.deposit(500);
+b1.withdraw(700)
 
 console.log(b1);
